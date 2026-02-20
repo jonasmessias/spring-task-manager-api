e7428a161929937d829f66def01727ab285f12c2
Jonas <jonasmessias30@gmail.com>
2026-02-20 12:06:45 -0300

    [REFAC]: Add nullability annotations to method parameters in CorsConfig and SecurityFilter, and reorder import in EmailService for consistency

--- src/main/java/com/example/taskmanagerapi/infra/security/SecurityFilter.java
+++ src/main/java/com/example/taskmanagerapi/infra/security/SecurityFilter.java
@@ -24,7 +24,9 @@ public class SecurityFilter extends OncePerRequestFilter {
     UserRepository userRepository;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@org.springframework.lang.NonNull HttpServletRequest request, 
+                                    @org.springframework.lang.NonNull HttpServletResponse response, 
+                                    @org.springframework.lang.NonNull FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
 
         if(token != null){
--- src/main/java/com/example/taskmanagerapi/services/EmailService.java
+++ src/main/java/com/example/taskmanagerapi/services/EmailService.java
@@ -1,9 +1,10 @@
 package com.example.taskmanagerapi.services;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
