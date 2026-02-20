795dae4ba2854a2725ff1a9a60ec2b48a7b5b15f
Jonas <jonasmessias30@gmail.com>
2026-02-20 12:06:45 -0300

    [REFAC]: Add nullability annotations to method parameters in CorsConfig and SecurityFilter, and reorder import in EmailService for consistency

--- src/main/java/com/example/taskmanagerapi/infra/cors/CorsConfig.java
+++ src/main/java/com/example/taskmanagerapi/infra/cors/CorsConfig.java
@@ -8,9 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@org.springframework.lang.NonNull CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedMethods("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
     }
 }
