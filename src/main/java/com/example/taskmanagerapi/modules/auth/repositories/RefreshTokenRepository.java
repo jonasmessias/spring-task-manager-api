be38aaccf7eeca038736a4d974e8a91aeb8cf1f7
Jonas <jonasmessias30@gmail.com>
2026-02-25 19:42:04 -0300

    [FEAT]: Add findByUser method to RefreshTokenRepository

--- src/main/java/com/example/taskmanagerapi/modules/auth/repositories/RefreshTokenRepository.java
+++ src/main/java/com/example/taskmanagerapi/modules/auth/repositories/RefreshTokenRepository.java
@@ -1,5 +1,6 @@
 package com.example.taskmanagerapi.modules.auth.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,6 +13,8 @@ import com.example.taskmanagerapi.modules.auth.domain.User;
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
     
     Optional<RefreshToken> findByToken(String token);
+    
+    List<RefreshToken> findByUser(User user);
     
     @Modifying
     @Transactional
