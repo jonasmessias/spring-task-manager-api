4cc0952af08cf29c9364e64f283426c556c97f99
Jonas <jonasmessias30@gmail.com>
2026-03-06 10:40:41 -0300

    [REFAC]: Add annotations for transactional and modifying behavior in EmailVerificationRepository

--- src/main/java/com/example/taskmanagerapi/modules/auth/repositories/EmailVerificationRepository.java
+++ src/main/java/com/example/taskmanagerapi/modules/auth/repositories/EmailVerificationRepository.java
@@ -3,10 +3,15 @@ package com.example.taskmanagerapi.modules.auth.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.taskmanagerapi.modules.auth.domain.EmailVerificationToken;
 
 public interface EmailVerificationRepository extends JpaRepository<EmailVerificationToken, String> {
     Optional<EmailVerificationToken> findByToken(String token);
+
+    @Modifying
+    @Transactional
     void deleteByEmail(String email);
 }
